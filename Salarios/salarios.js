473418a9f814feb0250c71d42c666b3f553e021d
Santiago_Ruberto <santiagorubertomagno@gmail.com>
2022-04-19 20:46:47 -0300

    Añadido la parte final

---
+++ Salarios/salarios.js
@@ -0,0 +1,46 @@
+const argentina = [];
+argentina.push({
+name: "Marcos Galperin",
+salary: 10000,
+});
+argentina.push({
+name: "Albert Einstein",
+salary: 700,
+});
+argentina.push({
+name: "Thomas Edison",
+salary: 870,
+});
+argentina.push({
+name: "Nikola Tesla",
+salary: 120,
+});
+argentina.push({
+name: "Elon Musk",
+salary: 20000,
+});
+argentina.push({
+name: "Steve Jobs",
+salary: 15000,
+});
+argentina.push({
+name: "Warren Buffet",
+salary: 8000,
+});
+argentina.push({
+name: "Jeff Bezos",
+salary: 9000,
+});
+argentina.push({
+name: "Bill Gates",
+salary: 11000,
+});
+argentina.push({
+name: "Mark Zuckerberg",
+salary: 7000,
+});
+argentina.push({
+name: "Pierpaolo Barbieri",
+salary: 1000,
+});
+
